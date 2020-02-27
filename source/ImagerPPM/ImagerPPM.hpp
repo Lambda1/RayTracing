424fd92438d929e71fd296f4ee2ed90fd4c71f01
lambda1 <breakthrough.dodorara@gmail.com>
2020-02-27 13:17:35 +0900

    hello ppm

--- source/ImagerPPM/ImagerPPM.hpp
+++ source/ImagerPPM/ImagerPPM.hpp
@@ -8,7 +8,9 @@ template<class TYPE>
 class ImagerPPM
 {
 	// Image Setting
+	using TYPE_OUT = unsigned int;
 	static inline constexpr unsigned int MAX_COLOR_VALUE = 255;
+	static inline constexpr float BIAS = 255.99;
 	const unsigned int m_width, m_height;
 	const std::string m_format;
 	public:
@@ -28,6 +30,16 @@ class ImagerPPM
 			std::cout << m_format << std::endl;
 			std::cout << m_width << " " << m_height << std::endl;
 			std::cout << MAX_COLOR_VALUE << std::endl;
+			for (int i = 0;i < m_height;++i)
+			{
+				for (int j = 0;j < m_width;++j)
+				{
+					TYPE r = static_cast<TYPE>(j)/m_width;
+					TYPE g = static_cast<TYPE>(i)/m_height;
+					TYPE b = static_cast<TYPE>(0.2f);
+					std::cout << static_cast<TYPE_OUT>(r*BIAS) << " " << static_cast<TYPE_OUT>(g*BIAS) << " " << static_cast<TYPE_OUT>(b*BIAS) << std::endl;
+				}
+			}
 		}
 };
 
